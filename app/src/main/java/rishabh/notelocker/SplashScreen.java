cf629a35bab67a937492fe1df12fc9c273fe8ec3
Rishabh <ojas14@gmail.com>
2019-03-29 23:59:08 +1100

    Added Splash Screen
    Added Splash Screen to the app and made changes to remove action bar

--- app/src/main/java/rishabh/notelocker/SplashScreen.java
+++ app/src/main/java/rishabh/notelocker/SplashScreen.java
@@ -1,5 +1,6 @@
 package rishabh.notelocker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,18 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Thread SplashSc = new Thread(){
+            public void run(){
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);//Go to MainActivity
+                    startActivity(intent);
+                    finish();//To finish the activity once another activity begins
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };//Thread to run splashScreen
+        SplashSc.start();//The thread SplashScreen thread will start
     }
 }
