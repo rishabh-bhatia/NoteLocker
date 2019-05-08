71d0ee0ca76e611437a245ec370a3addc9abe14f
Rishabh <ojas14@gmail.com>
2019-05-09 02:02:30 +1000

    Note Taking
    Note taking feature finalised with adding, saving and deleting note features added

--- app/src/main/java/rishabh/notelocker/EditNoteActivity.java
+++ app/src/main/java/rishabh/notelocker/EditNoteActivity.java
@@ -1,11 +1,18 @@
 package rishabh.notelocker;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 
+import java.util.HashSet;
+
 public class EditNoteActivity extends AppCompatActivity {
+    int noteRefId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +22,42 @@ public class EditNoteActivity extends AppCompatActivity {
         EditText editText = findViewById(R.id.editText4);
 
         Intent intent = getIntent();
-        int noteid = intent.getIntExtra("noteid", -1);
+        noteRefId = intent.getIntExtra("noteRefId", -1);
 
-        if(noteid != -1) {
-            editText.setText(Notes.notes.get(noteid));
+
+        if(noteRefId != -1) {
+            editText.setText(Notes.notes.get(noteRefId));
         }
+        //Add new notes
+        else{
+            Notes.notes.add("");
+            noteRefId = Notes.notes.size() - 1;
+            Notes.aa.notifyDataSetChanged();//Notifies the Array Adapter that data has changed
+        }
+
+        //Changes text in a specific note everytime that note's EditText changes text
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //No change required
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Notes.notes.set(noteRefId, String.valueOf(charSequence));
+                Notes.aa.notifyDataSetChanged();
+                //This is to get access to our app data
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("rishabh.notelocker", Context.MODE_PRIVATE);
+
+                HashSet<String> set = new HashSet(Notes.notes);
+                sharedPreferences.edit().putStringSet("notes", set).apply();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //No change Required
+            }
+        });
     }
 }
